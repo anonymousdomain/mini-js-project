9db77e4691921b63d94a02814e645fc626471b4b
anonymousdomain <domainanonymous1@gmail.com>
2021-11-18 22:05:12 +0300

    setTime out 3s

--- load-card/app.js
+++ load-card/app.js
@@ -1,23 +1,27 @@
-const Header=document.querySelector('#header')
-const title=document.querySelector('#title')
-const paragraph=document.querySelector('#paragraph')
-const profile_img=document.querySelector('#profile_img')
-const name=document.querySelector('#name')
-const date=document.querySelector('#date')
+const Header = document.querySelector('#header')
+const title = document.querySelector('#title')
+const paragraph = document.querySelector('#paragraph')
+const profile_img = document.querySelector('#profile_img')
+const name = document.querySelector('#name')
+const date = document.querySelector('#date')
 
-const animated_bg=document.querySelectorAll('.animated-bg')
-const animated_bg_text=document.querySelectorAll('.animated-bg-text')
+const animated_bg = document.querySelectorAll('.animated-bg')
+const animated_bg_text = document.querySelectorAll('.animated-bg-text')
 
 
-function getData(){
 
-    Header.innerHTML='<img src="https://randomuser.me/api/portraits/men/22.jpg" alt="">'
-title.innerHTML='hello this dawit'
-paragraph.innerHTML='hello this dave welcome to the site thanks for visiting'
-profile_img.innerHTML='<img src="https://randomuser.me/api/portraits/men/22.jpg" alt="">'
-name.innerHTML='Dawit Mekonnen'
-date.innerHTML='march 25 2022'
+function getData() {
 
-animated_bg.forEach(bg=>bg.classList.remove('animated-bg'))
-animated_bg_text.forEach(bg=>bg.classList.remove('animated-bg-text'))
+    Header.innerHTML = '<img src="https://randomuser.me/api/portraits/men/22.jpg" alt="">'
+    title.innerHTML = 'hello this dawit'
+    paragraph.innerHTML = 'hello this dave welcome to the site thanks for visiting'
+    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/22.jpg" alt="">'
+    name.innerHTML = 'Dawit Mekonnen'
+    date.innerHTML = 'march 25 2022'
+
+    animated_bg.forEach(bg => bg.classList.remove('animated-bg'))
+    animated_bg_text.forEach(bg => bg.classList.remove('animated-bg-text'))
+
 }
+
+setTimeout(getData,3000)

c419592d522b432123a25cd563ec8d60790135c0
anonymousdomain <domainanonymous1@gmail.com>
2021-11-18 19:20:36 +0300

    loading fecthed info from api

--- Live user Filter/app.js
+++ Live user Filter/app.js
@@ -1,13 +1,27 @@
-const filter=document.querySelector("#filter")
+const filter = document.querySelector("#filter")
 
-const user=document.querySelector(".user")
+const user = document.querySelector("#user")
 
-const listItems=[]
+const listItems = []
 
-async function getData(){
-    const res=await fetch('https://randomuser.me/api?results=30')
+getData()
 
-    const {results}=await res.json()
+async function getData() {
+    const res = await fetch('https://randomuser.me/api?results=30')
 
-    console.log(results)
+    const { results } = await res.json()
+    user.innerHTML = ''
+
+    results.forEach(user => {
+        const li = document.createElement('li')
+        listItems.push(li)
+        li.innerHTML = `
+   <img src="${user.picture.large}" alt="${user.name.last}"> 
+   <div class="user-info">
+   <h4>${user.name.first} ${user.name.last}</h4>
+   <p>${user.location.city},${user.location.country}</p>
+      </div>`
+
+        user.appendChild(li)
+    });
 }
